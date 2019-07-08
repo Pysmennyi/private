fd321e4c30a99c76c5e5ff96baa3076ff5b9864d
Pysmennyi <pismennyia@gmail.com>
2019-07-08 19:41:14 +0300

    fix function

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -10,9 +10,7 @@ let one =1;
 let two =2;
 const double_cicle = 2;
 const range_4 = 4;
-function getRandomArbitrary() {
-    return Math.random() * (max - min) + min;
-}
+let rand = Math.round(Math.random() * (max - min) + min);
 
 if(!ask) {
     alert('You did not become a billionaire, but can.');
@@ -21,25 +19,25 @@ if(!ask) {
             const enter_number_1 = prompt('Chose a roulette pocket number from 0 to 8 \nAttempts left: '
                 + attempts+ ' \nTotal price: ' + total_prise + '$ \nPossible price at current attempt: '
                 + prise1 + '$');
-            if (enter_number_1 === getRandomArbitrary()) {
+            if (enter_number_1 === rand) {
                 total_prise = total_prise + prise1;
                 alert('Congratulation, you won! Your prize is: ' + total_prise + ' $');
-            } else if (enter_number_1 !== getRandomArbitrary()) {
+            } else if (enter_number_1 !== rand) {
                 const enter_number_2 = prompt('Chose a roulette pocket number from 0 to 8 \nAttempts left: '
                     + (attempts-one) + ' \nTotal price: ' + total_prise + '$ \nPossible price at current attempt: '
                     + prise2 + '$');
-                if (enter_number_2 === getRandomArbitrary()) {
+                if (enter_number_2 === rand) {
                     total_prise = total_prise + prise2;
                     alert('Congratulation, you won! Your prize is:  ' + total_prise + ' 50$');
-                } else if (enter_number_2 !== getRandomArbitrary()) {
+                } else if (enter_number_2 !== rand) {
                     const enter_number_3 = prompt('Chose a roulette pocket number from 0 to 8 ' +
                         '\nAttempts left: ' + (attempts-two) + ' \nTotal price: ' + total_prise
                         + '$ \nPossible price at current attempt: ' + prise3 + '$');
-                    if (enter_number_3 === getRandomArbitrary()) {
+                    if (enter_number_3 === rand) {
                         total_prise = total_prise + prise3;
                         alert('Congratulation, you won! Your prize is:  ' + total_prise + ' $');
-                    } else if (enter_number_1 !== getRandomArbitrary() && enter_number_2 !== getRandomArbitrary()
-                        && enter_number_3 !== getRandomArbitrary()) {
+                    } else if (enter_number_1 !== rand && enter_number_2 !== rand
+                        && enter_number_3 !== rand) {
                         alert('Thank you for your participation. Your prize is: ' + total_prise +'$');
                         const ask_2 = confirm('Do you want to play again?');
                         if (!ask_2) {
