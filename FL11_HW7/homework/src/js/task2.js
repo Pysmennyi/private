2f69b142927fbe5d3abed2515203c2254bbdfe08
Pysmennyi <pismennyia@gmail.com>
2019-07-08 22:44:26 +0300

    edit

--- FL11_HW7/homework/src/js/task2.js
+++ FL11_HW7/homework/src/js/task2.js
@@ -1,4 +1,4 @@
-const ask = confirm('Do you want to play a game?');
+const ask = confirm(`Do you want to play a game?`);
 let prise1= 100;
 let prise2= 50;
 let prise3= 25;
@@ -13,33 +13,33 @@ const range_4 = 4;
 let rand = Math.round(Math.random() * (max - min) + min);
 
 if(!ask) {
-    alert('You did not become a billionaire, but can.');
+    alert(`You did not become a billionaire, but can.`);
 } else if (ask) {
         do {
-            const enter_number_1 = prompt('Chose a roulette pocket number from 0 to 8 \nAttempts left: '
-                + attempts+ ' \nTotal price: ' + total_prise + '$ \nPossible price at current attempt: '
-                + prise1 + '$');
+            const enter_number_1 = prompt(`Chose a roulette pocket number from 0 to 8 \nAttempts left: `
+                + attempts+ ` \nTotal price: ` + total_prise + `$ \nPossible price at current attempt: `
+                + prise1 + `$`);
             if (enter_number_1 === rand) {
                 total_prise = total_prise + prise1;
-                alert('Congratulation, you won! Your prize is: ' + total_prise + ' $');
+                alert(`Congratulation, you won! Your prize is: ` + total_prise + ` $`);
             } else if (enter_number_1 !== rand) {
-                const enter_number_2 = prompt('Chose a roulette pocket number from 0 to 8 \nAttempts left: '
-                    + (attempts-one) + ' \nTotal price: ' + total_prise + '$ \nPossible price at current attempt: '
-                    + prise2 + '$');
+                const enter_number_2 = prompt(`Chose a roulette pocket number from 0 to 8 \nAttempts left: `
+                    + (attempts-one) + ` \nTotal price: ` + total_prise + `$ \nPossible price at current attempt: `
+                    + prise2 + `$`);
                 if (enter_number_2 === rand) {
                     total_prise = total_prise + prise2;
-                    alert('Congratulation, you won! Your prize is:  ' + total_prise + ' 50$');
+                    alert(`Congratulation, you won! Your prize is:  ` + total_prise + ` 50$`);
                 } else if (enter_number_2 !== rand) {
-                    const enter_number_3 = prompt('Chose a roulette pocket number from 0 to 8 ' +
-                        '\nAttempts left: ' + (attempts-two) + ' \nTotal price: ' + total_prise
-                        + '$ \nPossible price at current attempt: ' + prise3 + '$');
+                    const enter_number_3 = prompt(`Chose a roulette pocket number from 0 to 8 ` +
+                        `\nAttempts left: ` + (attempts-two) + ` \nTotal price: ` + total_prise
+                        + `$ \nPossible price at current attempt: ` + prise3 + `$`);
                     if (enter_number_3 === rand) {
                         total_prise = total_prise + prise3;
-                        alert('Congratulation, you won! Your prize is:  ' + total_prise + ' $');
+                        alert(`Congratulation, you won! Your prize is:  ` + total_prise + ` $`);
                     } else if (enter_number_1 !== rand && enter_number_2 !== rand
                         && enter_number_3 !== rand) {
-                        alert('Thank you for your participation. Your prize is: ' + total_prise +'$');
-                        const ask_2 = confirm('Do you want to play again?');
+                        alert(`Thank you for your participation. Your prize is: ` + total_prise +`$`);
+                        const ask_2 = confirm(`Do you want to play again?`);
                         if (!ask_2) {
                             break;
                         } else if(ask) {
